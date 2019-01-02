8809934c7032b01d12cd1b09a76a773742bdf681
thr828 <592040808@qq.com>
2019-01-02 23:02:40 +0800

    Initial commit

---
+++ env5/urls.py
@@ -0,0 +1,23 @@
+"""env5 URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from Django_Web.views import index
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('index/', index), #url(r'^index/', index), #在url中凡是以url开头的访问都使用index函数来处理该请求
+]
